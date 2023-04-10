eaf7abfdb0bb0ae4bf4529a47609df9f667cb57f
Steffen Harbich <steffen@raubvogel.net>
2023-04-10 12:48:13 +0200

    run script: restart app on change of special file

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# fail build script when any command fails
+# fail build script when any command fails and kill background processes on script failure
 set -e
+trap 'sleep 5 && echo "Killing PIDs $(jobs -p)" && kill $(jobs -p)' ERR
 
 # configure application
 export NODE_ENV=production
@@ -10,8 +11,41 @@ export SP_PORT=4201
 export SP_CORS_ORIGIN=https://sp.rv-sys.de
 export SP_LOG_LEVEL=2
 
-# run via nodejs
+# change to dist folder relative to script location
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $DIR/dist
 
-node --max-old-space-size=128 server.js
+# run via nodejs
+echo "Starting application on $(date)"
+
+runApplication()
+{
+    node --max-old-space-size=128 server.js >> server.log &
+    APP_PID=$!
+}
+
+runApplication
+
+# listen for file changes of special file and restart application if modified
+LAST_MODIFIED=$(stat restart-on-modified.txt | grep Modify)
+
+while true; do
+    sleep 42
+    NEW_LAST_MODIFIED=$(stat restart-on-modified.txt | grep Modify)
+    if [ "$NEW_LAST_MODIFIED" != "$LAST_MODIFIED" ]
+    then
+        # restart application
+        echo "Restarting application on $(date)"
+        
+        if [ ! -z "$APP_PID" ]
+        then
+            # kill previous instance
+            kill $APP_PID
+            sleep 2
+        fi
+        
+        runApplication
+
+        LAST_MODIFIED=$NEW_LAST_MODIFIED
+    fi
+done
--- scrum-poker-server/src/logging.ts
+++ scrum-poker-server/src/logging.ts
@@ -6,5 +6,6 @@ export function createLogger(name: string) {
     hideLogPositionForProduction: true,
     minLevel: +(process.env.SP_LOG_LEVEL || 2),
     prettyLogTemplate: "{{yyyy}}.{{mm}}.{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}} {{logLevelName}} {{name}} ",
+    prettyLogTimeZone: "local",
   });
 }
