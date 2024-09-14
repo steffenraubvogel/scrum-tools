ac789af30ef9bf20df99b8f1316248a4ad95947d
Steffen Harbich <steffen@raubvogel.net>
2024-09-14 10:55:41 +0200

    fix nudge for observers + npm audit fix

--- scrum-tools-server/src/event-handlers/leader.ts
+++ scrum-tools-server/src/event-handlers/leader.ts
@@ -1,7 +1,7 @@
 import { Server, Socket } from "socket.io";
+import { ClientToServerEvents, ServerToClientEvents } from "../messages";
 import { ConnectionState, ServerState } from "../session";
 import { disconnectWithError } from "./common";
-import { ClientToServerEvents, ServerToClientEvents } from "../messages";
 
 export function handleLeaderReveal(ack: (err: string) => void, serverState: ServerState, connectionState: ConnectionState, socket: Socket) {
   const validationError = validateOperation(serverState, connectionState);
@@ -38,7 +38,7 @@ export function handleLeaderNudge(
   // and other status updates could also be a problem; need to handle this in a central place
 
   if (serverState[connectionState.pokerSessionId!].state === "guessing") {
-    const playersToNudge = serverState[connectionState.pokerSessionId!].players.filter((p) => p.guess === null);
+    const playersToNudge = serverState[connectionState.pokerSessionId!].players.filter((p) => p.type === "guesser" && p.guess === null);
     for (let player of playersToNudge) {
       socketio.to(connectionState.pokerSessionId! + "#" + player.name).emit("nudge");
     }
